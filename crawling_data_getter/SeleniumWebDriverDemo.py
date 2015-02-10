9144ab3386395bd8fcd8330b06dee6f862499a64
WENHAO MA <marvenhool911@hotmail.com>
2015-02-10 16:09:12 +0900

    main_Function Modified

--- crawling_data_getter/SeleniumWebDriverDemo.py
+++ crawling_data_getter/SeleniumWebDriverDemo.py
@@ -21,23 +21,24 @@ time.sleep(5)
 
 driver.execute_script('javascript:Dialog.searchList(13101)')
 time.sleep(10)
-#
+
 # #検索結果に１０番目ページに遷移
 driver.execute_script('javascript:List.pageList(10)')
 time.sleep(10)
-#
-#
-# #JS実行完了のページソースを取得
-#
 
 #JS実行完了のページソースを取得
 
+#タッグ内の値だけを取得
 body_text = driver.find_element_by_tag_name('body').text
 
+#JS実行後のソースを取得
 _html = driver.find_element_by_xpath('/html')
 html_text = _html.get_attribute('outerHTML')
 
+#エレメントIDでエレメントを取得
 checkbox_element = driver.find_element_by_id('MES06')
+
+
 
 #別コードセットからUTF-8コードに変更
 #html = buf.decode('shift_jis').encode('utf-8')
